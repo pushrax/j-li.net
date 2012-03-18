f683a38d7c36ab325fe13cd4de9ec37ba2b294d0
Justin Li <jli.justinli@gmail.com>
2012-03-17 21:37:24 -0500

    Initial AJAX support, updated menu generation, updated music page

---
+++ public/scripts/main.js
@@ -0,0 +1,43 @@
+function openPage(path, updateHistory) {
+	if (updateHistory) {
+		window.history.pushState('j-li.net-hist|' + path, 'Title', path);
+	}
+	
+	$('#content').fadeOut(100, function() {
+		$.getJSON(path, function(data) {
+			if (data.view == 'index') $('#submenu').fadeOut(500);
+			else $('#submenu').fadeIn(500);
+			
+			$('#content').html(data.html).fadeIn(100);
+			$('#submenu li').removeClass('active');
+			$('#submenu li.' + data.view).addClass('active');
+			
+			updateClickHandlers();
+			if (data.title !== '') document.title = data.title + ' >> j-li.net';
+			else document.title = 'j-li.net';
+		});
+	});
+}
+
+$(document).ready(function() {
+	if (Modernizr.history) {
+		window.history.replaceState('j-li.net-hist|', 'Title', window.location.pathname);
+		$('a.ajaxlink').click(function() {
+			openPage($(this).attr('href'), true);
+			return false;
+		});
+	}
+});
+
+function updateClickHandlers() {
+	$('#content a.ajaxlink').click(function() {
+		openPage($(this).attr("href"), true);
+		return false;
+	});
+}
+
+window.onpopstate = function(event) {
+	if (event.state.substr(0, 14) == 'j-li.net-hist|') {
+		openPage(event.state.substr(14), false);
+	}
+};
--- routes.js
+++ routes.js
@@ -1,25 +1,43 @@
 var fs = require('fs');
 
-module.exports = function(app){
+module.exports = function(app) {
 	/*fs.readdirSync(__dirname).forEach(function(file) {
 		if (file == "index.js") return;
 		
 		require('./' + file.substr(0, file.indexOf('.')))(app);
 	});*/
+
+	function renderPage(req, res, view, title) {
+		var menuItems = [
+			{ name: 'projects', path: '/projects', ajax: true },
+			{ name: 'music', path: '/music', ajax: true },
+			{ name: 'resume', path: '/resume', ajax: false },
+			{ name: 'contact', path: '/contact', ajax: true }
+		];
+		
+		var ajaxRequest = (typeof req.query['ajax'] !== "undefined");
+		if (req.xhr || ajaxRequest) {
+			res.partial(view, { title: title, view: view, menuItems: menuItems }, function(err, str) {
+				res.json({title: title, view: view, html: str});
+			});
+		} else {
+			res.render(view, { title: title, view: view, menuItems: menuItems });
+		}
+	}
 	
-	app.get('/', function(req, res){
-		res.render('index', { title: '' });
+	app.get('/', function(req, res) {
+		renderPage(req, res, 'index', '');
 	});
 
 	app.get('/projects', function(req, res) {
-		res.render('projects', { title: 'Projects' });
+		renderPage(req, res, 'projects', 'Projects');
 	});
 
 	app.get('/contact', function(req, res) {
-		res.render('contact', { title: 'Contact' });
+		renderPage(req, res, 'contact', 'Contact');
 	});
 
 	app.get('/music', function(req, res) {
-		res.render('music', { title: 'Music' });
+		renderPage(req, res, 'music', 'Music');
 	});
 };
