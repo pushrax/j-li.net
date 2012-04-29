7c5e1e42588ff8e8923016ca56548bb2d75d802d
Justin Li <jli.justinli@gmail.com>
2012-04-28 23:52:38 -0500

    Removed useless session support

--- j-li.js
+++ j-li.js
@@ -9,8 +9,6 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'iNiTpbjqhpYAokshkgrrW0GmId6uRmcu' }));
   app.use(app.router);
   app.use('/', express.static(__dirname + '/public'));
 });
