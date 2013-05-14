b3a96c012e8cdceacab7dfee71e81788da935027
Justin Li <jli.justinli@gmail.com>
2013-05-14 01:12:53 -0400

    Updated site design to improve consistency

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,61 +1,61 @@
 function openPage(path, updateHistory) {
-	if (updateHistory) {
-		window.history.pushState('j-li.net-hist|' + path, 'Title', path);
-	}
-	
-	$('#content-wrapper').animate({ opacity: 0 }, 100, function() {
-		$.getJSON(path + "?ajax", function(data) {
-			$('body').removeClass().addClass(data.view);			
-			$('#content-wrapper').html(data.html).animate({ opacity: 1 }, 100);
-			updateClickHandlers(false);
-			if (updateHistory) window.scrollTo(0, 1);
-			if (data.title !== '') document.title = data.title + ' » j-li.net';
-			else document.title = 'j-li.net';
-		});
-	});
+  if (updateHistory) {
+    window.history.pushState('j-li.net-hist|' + path, 'Title', path);
+  }
+
+  $('#content-wrapper').animate({ opacity: 0 }, 100, function() {
+    $.getJSON(path + "?ajax", function(data) {
+      $('body').removeClass().addClass(data.view);
+      $('#content-wrapper').html(data.html).animate({ opacity: 1 }, 100);
+      updateClickHandlers(false);
+      if (updateHistory) window.scrollTo(0, 1);
+      if (data.title !== '') document.title = data.title + ' » Justin Li';
+      else document.title = 'Justin Li';
+    });
+  });
 }
 
 var stratus = false;
 
 function startStratus(href) {
-	if (stratus) return;
-	$.stratus({
-		color: 'd91e76',
-		links: href,
-		//animate: false,
-		auto_play: true,
-		volume: 100
-		//theme: 'http://stratus.sc/themes/dark.css'
-	});
-	stratus = true;
+  if (stratus) return;
+  $.stratus({
+    color: 'd91e76',
+    links: href,
+    //animate: false,
+    auto_play: true,
+    volume: 100
+    //theme: 'http://stratus.sc/themes/dark.css'
+  });
+  stratus = true;
 }
 
 function updateClickHandlers(globalAjax) {
-	$(globalAjax ? 'a.ajax' : '#content a.ajax').click(function() {
-		openPage($(this).attr("href"), true);
-		$(this).blur();
-		return false;
-	});
+  $(globalAjax ? 'a.ajax' : '#content a.ajax').click(function() {
+    openPage($(this).attr("href"), true);
+    $(this).blur();
+    return false;
+  });
 
-	if (stratus) $.stratus.bindClickHandler();
-	else $('a.stratus').click(function() {
-		$('a.stratus').unbind('click');
-		startStratus($(this).attr('href'));
-		return false;
-	});
+  if (stratus) $.stratus.bindClickHandler();
+  else $('a.stratus').click(function() {
+    $('a.stratus').unbind('click');
+    startStratus($(this).attr('href'));
+    return false;
+  });
 }
 
 window.onpopstate = function(event) {
-	if (event.state && event.state.substr(0, 14) == 'j-li.net-hist|') {
-		openPage(event.state.substr(14), false);
-	}
+  if (event.state && event.state.substr(0, 14) == 'j-li.net-hist|') {
+    openPage(event.state.substr(14), false);
+  }
 };
 
 $(document).ready(function() {
-	if (Modernizr.history) {
-		window.history.replaceState('j-li.net-hist|', 'Title', window.location.pathname);
-		updateClickHandlers(true);
-	}
+  if (Modernizr.history) {
+    window.history.replaceState('j-li.net-hist|', 'Title', window.location.pathname);
+    updateClickHandlers(true);
+  }
 
   SC.initialize({
     client_id: "77272076b9c38d199fad773ce817a2fc"
